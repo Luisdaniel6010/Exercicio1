50f628c9f7402ba0bbadb2e886558bbae0d0b3f5
luis <6010ldaniel@gmail.com>
2022-11-27 13:08:48 -0300

    index.css

--- valid.js
+++ valid.js
@@ -4,18 +4,24 @@ document.addEventListener("DOMContentLoaded", function(){
 
 function validarFormulario(evento) {
     evento.preventDefault();
+
     let nombre = document.getElementById('nombre').value;
-    if(nombre.length == 0) {
-        console.log('nombre vacio');
+    let nomeregex = /([A-z]+\s)+/;
+    
+    if(nombre === nomeregex) {
+        console.log(nombre);
+        document.getElementById('nombre').focus();
+        document.getElementById('nombre').value="";
         return;
     }
     let email = document.getElementById('email').value;
-    let regex = /^\w+([-+.']\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*$/;
+    let emailregex = /\w+@\w+\.\w+/;
 
-    if (email != regex) {
+    if(email != emailregex) {
+
         alert('E-mail no es valido');
         document.getElementById('email').focus();
-        document.getElementById('email').value(" ");
+        document.getElementById('email').value="";
 
     }
     this.submit();
